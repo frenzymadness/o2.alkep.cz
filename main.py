176171d47b6fdad4fa4221ebe8a42a42da06951a
Lumir Balhar <lbalhar@redhat.com>
2019-03-25 18:02:24 +0100

    Make it more DRY

--- main.py
+++ main.py
@@ -13,17 +13,12 @@ app.secret_key = 'super secret key'
 root = path.dirname(path.abspath(__file__))
 
 
-def new_person(name="???", group="???"):
-    return {
-        "name": name,
-        "group": group,
-        "usage": 0.0,
-        "regular": 0.0,
-        "one_time": 0.0,
-        "payments": 0.0,
-        "services": 0.0,
-        "sum": 0.0
-    }
+def new_person(name="???", group="???", types=None):
+    person = {"name": name, "group": group, "sum": 0.0}
+    for type in types:
+        person[type] = 0.0
+
+    return person
 
 
 def load_persons():
@@ -85,76 +80,52 @@ def process():
     else:
         tree = ET.parse(file)
 
+    to_process = [
+        ("regular", "regularCharges", "rcTotalPriceWithVat"),
+        ("usage", "usageCharges", "ucTotalPriceWithVat"),
+        ("payments", "payments", "paymentTotalPrice"),
+        ("one_time", "oneTimeCharges", "otcTotalPriceWithVat"),
+        ("services", "additionalServices", "asTotalPriceWithVat"),
+    ]
+
+    types = [type for type, _, _ in to_process]
+
+    # For loop to get data about every customer/phone number
     for customer in tree.findall(".//subscriber[@accountType='CUST']"):
         phone = customer.attrib["phoneNumber"]
         try:
             person = persons[phone]
         except KeyError:
-            person = new_person()
+            person = new_person(types=types)
             persons[phone] = person
-        try:
-            rc = customer.findall(".//regularCharges")[0]
-            person["regular"] += float(rc.attrib["rcTotalPriceWithVat"])
-        except IndexError:
-            pass
-        try:
-            uc = customer.findall(".//usageCharges")[0]
-            person["usage"] += float(uc.attrib["ucTotalPriceWithVat"])
-        except IndexError:
-            pass
-        try:
-            pay = customer.findall(".//payments")[0]
-            person["payments"] += float(pay.attrib["paymentTotalPrice"])
-        except IndexError:
-            pass
-        try:
-            one_time = customer.findall(".//oneTimeCharges")[0]
-            person["one_time"] += float(one_time.attrib["otcTotalPriceWithVat"])
-        except IndexError:
-            pass
-        try:
-            services = customer.findall(".//additionalServices")[0]
-            person["services"] += float(services.attrib["asTotalPriceWithVat"])
-        except IndexError:
-            pass
 
-        person["sum"] = sum([person["regular"],
-                             person["usage"],
-                             person["payments"],
-                             person["one_time"],
-                             person["services"]])
+        for type, tag, attrib in to_process:
+            try:
+                rc = customer.findall(".//" + tag)[0]
+                person[type] += float(rc.attrib[attrib])
+            except IndexError:
+                pass
 
+            person["sum"] += person[type]
+
+    # Groups of customers
     groups = defaultdict(lambda: defaultdict(float))
     for phone, person in persons.items():
         group = person["group"]
-        groups[group]["usage"] += person["usage"]
-        groups[group]["regular"] += person["regular"]
-        groups[group]["payments"] += person["payments"]
-        groups[group]["one_time"] += person["one_time"]
-        groups[group]["services"] += person["services"]
-
-        groups[group]["sum"] += sum([person["regular"],
-                                     person["usage"],
-                                     person["payments"],
-                                     person["one_time"],
-                                     person["services"]])
+        for type, _, _ in to_process:
+            groups[group][type] += person[type]
+            groups[group]["sum"] += person[type]
 
     groups = OrderedDict(sorted(groups.items()))
 
     # Summaries (last line of each table)
     sums = {}
-    sums["persons_regular"] = sum([p["regular"] for _, p in persons.items()])
-    sums["persons_usage"] = sum([p["usage"] for _, p in persons.items()])
-    sums["persons_payments"] = sum([p["payments"] for _, p in persons.items()])
-    sums["persons_one_time"] = sum([p["one_time"] for _, p in persons.items()])
-    sums["persons_services"] = sum([p["services"] for _, p in persons.items()])
+    for type, _, _ in to_process:
+        sums["persons_" + type] = sum([p[type] for _, p in persons.items()])
     sums["all_persons"] = sum([v for v in sums.values()])
 
-    sums["group_regular"] = sum([g["regular"] for _, g  in groups.items()])
-    sums["group_usage"] = sum([g["usage"] for _, g  in groups.items()])
-    sums["group_payments"] = sum([g["payments"] for _, g  in groups.items()])
-    sums["group_one_time"] = sum([g["one_time"] for _, g  in groups.items()])
-    sums["group_services"] = sum([g["services"] for _, g  in groups.items()])
+    for type, _, _ in to_process:
+        sums["group_" + type] = sum([g[type] for _, g in groups.items()])
     sums["all_groups"] = sum([v
                               for k, v in sums.items()
                               if k.startswith("group_")])
